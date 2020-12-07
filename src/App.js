5a47747c8822fe15ff293aab3e5a031dbe4b1197
Chris Demetriad <chris@demetriad.co.uk>
2020-12-07 23:13:58 +0000

    Getting star system details

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 
 function App() {
-	const [stars, setStars] = useState([]);
+	const [starSystem, setStarSystems] = useState([]);
+	const [starDetails, setStarDetails] = useState([]);
+
 	console.log(process.env.REACT_APP_STARS_API);
-	const loadSystems = () => {
-		fetch(process.env.REACT_APP_STARS_API)
+	const loadStarSystems = () => {
+		fetch(process.env.REACT_APP_STARS_API + "?size=40")
 			.then((res) => {
 				return res.json();
 			})
 			.then((data) => {
-				setStars(data._embedded.stars);
+				setStarSystems(data._embedded.stars);
 			})
 			.catch((error) => {
 				console.log(error);
@@ -18,17 +20,39 @@ function App() {
 	};
 
 	useEffect(() => {
-		loadSystems();
+		loadStarSystems();
 	}, []);
+
+	const loadStarDetails = (url) => {
+		fetch(url)
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				setStarDetails(data._embedded.planets);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
 	return (
 		<div className="App">
-			{stars
+			{starDetails.length > 0 && <p>{starDetails[0].name}</p>}
+			{starSystem
 				.sort((a, b) => (a.numberOfPlanets < b.numberOfPlanets ? 1 : -1))
 				.map((star, index) => {
 					return (
-						<p key={index}>
-							<span>Star system name: {star.name}</span>, <span>{star.numberOfPlanets} planets</span>, <span>{star.distance} years from Earth.</span>
+						<p
+							key={index}
+							onClick={() => {
+								loadStarDetails(star._links.planets.href);
+							}}
+						>
+							<span>
+								[{index}] Star system name: {star.name}
+							</span>
+							, <span>{star.numberOfPlanets} planets</span>, <span>{star.distance} years from Earth.</span>, <span>{star._links.planets.href}</span>
 						</p>
 					);
 				})}

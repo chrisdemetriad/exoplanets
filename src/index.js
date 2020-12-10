bfd4ecf52e74b4e3c1fc41c44a8685bd6d45a3b0
Chris Demetriad <chris@demetriad.co.uk>
2020-12-10 03:20:39 +0000

    Adding a slider on the Search page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Have removed StrictMode to get around findDOMNode being deprecated in StrictMode so I can keep the console nice and clear
+// https://github.com/react-component/slider/issues/613 - in real life, removing it shouldn't be a solution as it helps with
+// identifying components with unsafe lifecycles, warns about various API items usage and deprecations, detects unexpected side effects, etc

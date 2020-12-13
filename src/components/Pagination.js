64c7f397512ce0cacbaba9f60cb6ab2bed3bca4d
Chris Demetriad <chris@demetriad.co.uk>
2020-12-13 02:07:55 +0000

    Minor pagination fix

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,20 +2,24 @@ import React from "react";
 
 function Pagination({ page, lastPage, loadStarSystems, planetsNumber }) {
 	return (
-		<div>
+		<div className="pagination">
 			<button disabled={page === 0} onClick={() => loadStarSystems(0, planetsNumber)}>
 				First
 			</button>
 			<button disabled={page === 0} onClick={() => loadStarSystems(page - 1, planetsNumber)}>
 				Previous
 			</button>
-			<span>
-				Page {page} from {lastPage}
-			</span>
-			<button disabled={page === lastPage} onClick={() => loadStarSystems(page + 1, planetsNumber)}>
+			{page !== 0 ? (
+				<span className="page-info">
+					Page {page} from {lastPage}
+				</span>
+			) : (
+				<span className="page-info">Pages: {lastPage}</span>
+			)}
+			<button disabled={page === lastPage - 1} onClick={() => loadStarSystems(page + 1, planetsNumber)}>
 				Next
 			</button>
-			<button disabled={page === lastPage} onClick={() => loadStarSystems(lastPage, planetsNumber)}>
+			<button disabled={page === lastPage - 1} onClick={() => loadStarSystems(lastPage - 1, planetsNumber)}>
 				Last
 			</button>
 		</div>
